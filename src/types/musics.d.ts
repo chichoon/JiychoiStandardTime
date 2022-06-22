11069d366ae52e19ab42485f2a7c0d3c3fd67302
chichoon <chichoon.choi@gmail.com>
2022-06-23 03:16:37 +0900

    Feat: 음악리스트 소소하게 수정, 태그 추가

--- src/types/musics.d.ts
+++ src/types/musics.d.ts
@@ -7,6 +7,23 @@ export interface ISong {
   title: string;
   artist: string;
   comment: string;
-  url: string;
+  id: string;
   date: string;
+  recommended?: string;
+  tag: string[];
 }
+
+/*
+  TODO: 생각중인 태그 목록
+  신나요 (excited)
+  차분해요 (peaceful)
+  코딩해요 (coding)
+  빨라요 (fast)
+  느려요 (slow)
+  행복해요 (happy)
+  화나요 (angry)
+  슬퍼요 (sad)
+  시끄러워요 (loud)
+  조용해요 (quiet)
+  보컬이 없어요 (inst)
+  */
