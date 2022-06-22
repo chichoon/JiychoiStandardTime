5660bc04cf607ddfa78ba695c0f12b469afa0960
chichoon <chichoon.choi@gmail.com>
2022-06-23 04:49:03 +0900

    Design: 태그박스 디자인

--- src/components/PlayerComponent/TagBox/index.tsx
+++ src/components/PlayerComponent/TagBox/index.tsx
@@ -1,12 +1,14 @@
 import { Tag } from 'components';
 
+import styles from './tagBox.module.scss';
+
 interface IProps {
   tagList: string[];
 }
 
 const TagBox = ({ tagList }: IProps) => {
   return (
-    <ul>
+    <ul className={styles.tagBoxWrapper}>
       {tagList.map((tag) => (
         <li key={`tag-${tag}`}>
           <Tag tagName={tag} />
