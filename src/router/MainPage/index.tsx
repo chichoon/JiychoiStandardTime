098eb84edd75aba7d8304aaf581b4d43f932d7c3
chichoon <chichoon.choi@gmail.com>
2022-06-23 02:00:30 +0900

    Rename: MainPage => Main (페이지가 아니라 컴포넌트가 렌더링되므로)

--- src/router/MainPage/index.tsx
+++
@@ -1,5 +0,0 @@
-const MainPage = () => {
-  return <div>아아아</div>;
-};
-
-export default MainPage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,12 @@
 import Layout from 'components';
 import { Route, Routes } from 'react-router-dom';
-import MainPage from './MainPage';
+import Main from './Main';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route path='/' element={<MainPage />} />
+        <Route path='/' element={<Main />} />
       </Route>
     </Routes>
   );
