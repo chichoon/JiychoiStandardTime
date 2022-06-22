cd8b85ab6d32d4fccd85f0e901af20ccfd635d63
chichoon <chichoon.choi@gmail.com>
2022-06-23 01:18:58 +0900

    Design: 초기 디자인 리셋하는 scss 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from 'router';
-
 import reportWebVitals from './reportWebVitals';
+
+import 'styles/index.scss';
+import 'styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
