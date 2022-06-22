88ec5710f89411b2dbb5e6d91d001cb91dd62f4c
chichoon <chichoon.choi@gmail.com>
2022-06-23 02:43:28 +0900

    Feat: 루트 경로는 main으로 리디렉션

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 import Layout from 'components';
-import { Route, Routes } from 'react-router-dom';
 import Main from './Main';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={<Navigate replace to='main' />} />
+        <Route path='main' element={<Main />} />
+        <Route path='mood' element={<Main />} />
+        <Route path='custom' element={<Main />} />
       </Route>
     </Routes>
   );
