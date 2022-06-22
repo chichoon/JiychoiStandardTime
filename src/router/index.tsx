20c5aa48b93c287de9834b6f20a29087b4c6c1b0
chichoon <chichoon.choi@gmail.com>
2022-06-23 01:32:13 +0900

    Feat: Layout 적용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,13 @@
+import Layout from 'components';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<MainPage />} />
+      </Route>
     </Routes>
   );
 };
