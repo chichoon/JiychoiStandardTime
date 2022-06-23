ea43a3f2699779b8f5012c0e255c86c4000cb743
chichoon <chichoon.choi@gmail.com>
2022-06-23 11:39:02 +0900

    Remove: 불필요한 주석 삭제

--- src/types/musics.d.ts
+++ src/types/musics.d.ts
@@ -12,18 +12,3 @@ export interface ISong {
   recommended?: string;
   tagList: string[];
 }
-
-/*
-  TODO: 생각중인 태그 목록
-  신나요 (excited)
-  차분해요 (peaceful)
-  코딩해요 (coding)
-  빨라요 (fast)
-  느려요 (slow)
-  행복해요 (happy)
-  화나요 (angry)
-  슬퍼요 (sad)
-  시끄러워요 (loud)
-  조용해요 (quiet)
-  보컬이 없어요 (inst)
-  */
