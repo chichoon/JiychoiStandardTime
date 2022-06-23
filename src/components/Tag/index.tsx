15b9837fdcab138e833e9e81cc0ba5426ae4b30e
chichoon <chichoon.choi@gmail.com>
2022-06-23 13:54:26 +0900

    Feat: 데이터 업데이트 (~211113)

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -10,15 +10,21 @@ interface IProps {
 const Tag = ({ tagName, onClick }: IProps) => {
   const korTagName = {
     excited: '신나요',
+    loud: '시끄러워요',
     peaceful: '차분해요',
     coding: '코딩중',
-    fast: '빨라요',
-    slow: '느려요',
-    happy: '행복해요',
-    angry: '화나요',
-    sad: '슬퍼요',
-    loud: '시끄러워요',
-    quiet: '조용해요',
+    fast: '속도감 있어요',
+    slow: '느긋해요',
+    woman: '여자 보컬',
+    man: '남자 보컬',
+    both: '혼성 보컬',
+    full: '풍성해요',
+    live: '라이브 음원',
+    band: '밴드 사운드',
+    orchestra: '오케스트라 사운드',
+    guitar: '훌륭한 기타',
+    piano: '경쾌한 피아노',
+    bass: '끝내주는 베이스',
     inst: '보컬이 없어요',
   }[tagName];
 
