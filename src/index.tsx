9ec6d8fc780ea71f0ee3e4a335f14208d518d0a9
chichoon <chichoon.choi@gmail.com>
2022-06-23 14:15:44 +0900

    Feat: React-query 루트에 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import Router from 'router';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +10,22 @@ import reportWebVitals from './reportWebVitals';
 import 'styles/index.scss';
 import 'styles/global.scss';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Router />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
