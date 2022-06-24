e41579d88e17c01f0f2418cc93f6f1b227824b45
chichoon <chichoon.choi@gmail.com>
2022-06-24 19:19:03 +0900

    Design: 노래 제목 - 가수명 스타일 global 스타일로 빼기

--- src/router/Mood/FilteredSongList/FilteredSongElement.tsx
+++ src/router/Mood/FilteredSongList/FilteredSongElement.tsx
@@ -1,3 +1,5 @@
+import cx from 'classnames';
+
 import { ISong } from 'types/musics';
 import styles from './filteredSongList.module.scss';
 
@@ -6,7 +8,14 @@ interface IProps {
 }
 
 const FilteredSongElement = ({ song }: IProps) => {
-  return <li className={styles.filteredSongElement}>{song.title}</li>;
+  return (
+    <li className={styles.filteredSongElement}>
+      <dl className={cx('currentSongInfo')}>
+        <dt>{song.title}</dt>
+        <dd>{song.artist}</dd>
+      </dl>
+    </li>
+  );
 };
 
 export default FilteredSongElement;
