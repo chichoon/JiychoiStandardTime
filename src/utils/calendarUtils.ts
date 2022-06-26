efd0461d351bbfab343f0f0962ced9098d46650b
chichoon <chichoon.choi@gmail.com>
2022-06-26 18:57:33 +0900

    Refactor: getMonthArray 함수에서 날짜 전체 계산하도록 변경

--- src/utils/calendarUtils.ts
+++ src/utils/calendarUtils.ts
@@ -1,14 +1,16 @@
 import { Dayjs } from 'dayjs';
 
 interface IMonthArray {
-  lastMonthArr: Array<number>;
-  thisMonthArr: Array<number>;
-  nextMonthArr: Array<number>;
+  lastMonthArr: Array<Dayjs>;
+  thisMonthArr: Array<Dayjs>;
+  nextMonthArr: Array<Dayjs>;
   numOfDate: number;
 }
 
 export const getMonthArray = (date: Dayjs) => {
-  const lastMonthLastDay = date.add(-1, 'month').daysInMonth();
+  const lastMonth = date.add(-1, 'month');
+  const nextMonth = date.add(1, 'month');
+  const lastMonthLastDay = lastMonth.daysInMonth();
   const firstDay = date.startOf('month').day();
   const thisMonthLastDay = date.daysInMonth();
   const nextMonthDay = 7 - ((firstDay + thisMonthLastDay) % 7);
@@ -19,9 +21,9 @@ export const getMonthArray = (date: Dayjs) => {
     numOfDate: 0,
   };
 
-  for (let i = firstDay - 1; i >= 0; i -= 1) monthArray.lastMonthArr.push(lastMonthLastDay - i);
-  for (let i = 1; i <= thisMonthLastDay; i += 1) monthArray.thisMonthArr.push(i);
-  if (nextMonthDay !== 7) for (let i = 1; i <= nextMonthDay; i += 1) monthArray.nextMonthArr.push(i);
+  for (let i = firstDay - 1; i >= 0; i -= 1) monthArray.lastMonthArr.push(lastMonth.date(lastMonthLastDay - i));
+  for (let i = 1; i <= thisMonthLastDay; i += 1) monthArray.thisMonthArr.push(date.date(i));
+  if (nextMonthDay !== 7) for (let i = 1; i <= nextMonthDay; i += 1) monthArray.nextMonthArr.push(nextMonth.date(i));
   monthArray.numOfDate =
     monthArray.lastMonthArr.length + monthArray.thisMonthArr.length + monthArray.nextMonthArr.length;
   return monthArray;
