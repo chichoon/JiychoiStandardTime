823314143aa6f12ce62480aaeaf8dcd273ea66be
chichoon <chichoon.choi@gmail.com>
2022-06-26 16:04:18 +0900

    Fix: jst-songlist 루트 폴더에서 불러오기 (경로 문제 수정)

--- src/hooks/useFetchSongByIndex.ts
+++ src/hooks/useFetchSongByIndex.ts
@@ -3,11 +3,12 @@ import { useQuery } from 'react-query';
 import { getSongByIndex } from 'services';
 
 const useFetchSongByIndex = (index: number) => {
-  const { data = INIT_DATA } = useQuery(['latestSong'], () => getSongByIndex(index), {
+  const { data = INIT_DATA } = useQuery(['indexSong', index], () => getSongByIndex(index), {
     refetchOnWindowFocus: false,
     staleTime: 1000 * 60 * 60 * 12,
     cacheTime: 1000 * 60 * 60 * 12,
     retry: false,
+    enabled: !!index,
     useErrorBoundary: true,
     suspense: true,
   });
--- src/services/getAllSongs.ts
+++ src/services/getAllSongs.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ISong } from 'types/musics';
 
 const getAllSongs = (): Promise<ISong[]> =>
-  axios.get('jst-songlist.json').then((response) => {
+  axios.get('/jst-songlist.json').then((response) => {
     const allSongsList: ISong[] = response.data.songs;
 
     return allSongsList;
