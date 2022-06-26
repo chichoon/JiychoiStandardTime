ff10592a8b28b5378c47f2d481c5bf367ccdc53d
chichoon <chichoon.choi@gmail.com>
2022-06-26 22:43:50 +0900

    Refactor: 굳이 필요없는 CalendarInner 제거

--- src/router/Calendar/CalendarInner.tsx
+++
@@ -1,7 +0,0 @@
-import OverviewCalendar from './OverviewCalendar';
-
-const CalendarInner = () => {
-  return <OverviewCalendar />;
-};
-
-export default CalendarInner;
--- src/router/Calendar/index.tsx
+++ src/router/Calendar/index.tsx
@@ -1,18 +1,20 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import cx from 'classnames';
 
 import { Error, Loading } from 'components';
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import CalendarInner from './CalendarInner';
+import OverviewCalendar from './OverviewCalendar';
+
+import styles from './calendar.module.scss';
 
 const Calendar = () => {
   const handleErrorFallback = () => <Error message='달력을 불러올 수 없어요' />;
 
   return (
-    <div className={cx('innerContainer')}>
+    <div className={cx('innerContainer', styles.calendarWrapper)}>
       <ErrorBoundary FallbackComponent={handleErrorFallback}>
         <Suspense fallback={<Loading />}>
-          <CalendarInner />
+          <OverviewCalendar />
         </Suspense>
       </ErrorBoundary>
     </div>
