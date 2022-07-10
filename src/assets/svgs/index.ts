168c91203fa6a23a0e60f63ae5a5b05744f914b0
chichoon <chichoon.choi@gmail.com>
2022-07-11 01:43:24 +0900

    Feat: 테마 변경 기능 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,4 @@
 export { ReactComponent as ErrorIcon } from './errorIcon.svg';
 export { ReactComponent as TriangleIcon } from './triangleIcon.svg';
+export { ReactComponent as SunIcon } from './sunIcon.svg';
+export { ReactComponent as MoonIcon } from './moonIcon.svg';
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,35 @@
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHoverDirty } from 'react-use';
 import { Outlet } from 'react-router-dom';
 
+import { toggleTheme } from 'states/colorTheme';
 import NavSection from './NavSection';
 
+import { MoonIcon, SunIcon } from 'assets/svgs';
 import styles from './layout.module.scss';
 
 const Layout = () => {
+  const imageRef = useRef(null);
+  const isHovering = useHoverDirty(imageRef);
+  const dispatch = useDispatch();
+
+  const handleThemeButtonClick = () => {
+    dispatch(toggleTheme());
+  };
+
   return (
     <div className={styles.layoutBackground}>
+      <button type='button' className={styles.hoverButton} onClick={handleThemeButtonClick}>
+        <div className={styles.buttonSun}>
+          <SunIcon />
+        </div>
+        <div className={styles.buttonMoon}>
+          <MoonIcon />
+        </div>
+      </button>
       <div className={styles.layoutWrapper}>
-        <img src='/main-image.gif' alt='main-profile' />
+        <img ref={imageRef} src={isHovering ? '/question-image.png' : '/main-image.gif'} alt='main-profile' />
         <div className={styles.layoutContainer}>
           <h1 className={styles.layoutHeader}>#지최표준시</h1>
           <p className={styles.layoutSubHeader}>jiychoi standard time</p>
