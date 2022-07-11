c557dfe2c12ea2718759920b5823e6ad0ed12744
chichoon <chichoon.choi@gmail.com>
2022-07-11 18:51:00 +0900

    Design: 오늘 날짜는 그냥 볼드처리만

--- src/router/Calendar/OverviewCalendar/CalendarBody/CalendarDateElement.tsx
+++ src/router/Calendar/OverviewCalendar/CalendarBody/CalendarDateElement.tsx
@@ -21,6 +21,7 @@ const CalendarDateElement = ({ date, songOfTheDay, ifOtherMonth }: IProps) => {
   const buttonRef = useRef(null);
   const isHovering = useHoverDirty(buttonRef);
   const tooltipString = songOfTheDay ? `${songOfTheDay.date}\n${songOfTheDay.title}` : '';
+  const isToday = today.isSame(date, 'day');
 
   return (
     <li className={styles.calendarButtonWrapper}>
@@ -31,15 +32,13 @@ const CalendarDateElement = ({ date, songOfTheDay, ifOtherMonth }: IProps) => {
             className={cx(styles.dateWrapper, styles.songAvailable, { [styles.otherMonthDate]: ifOtherMonth })}
             ref={buttonRef}
           >
-            {date.date()}
-            {today.isSame(date) && <div className={styles.today}>오늘</div>}
+            <span className={cx({ [styles.today]: isToday })}>{date.date()}</span>
           </Link>
           <Tooltip tooltipString={tooltipString} position='topMiddle' isShown={isHovering} />
         </>
       ) : (
         <div className={cx(styles.dateWrapper, { [styles.otherMonthDate]: ifOtherMonth })}>
-          {date.date()}
-          {today.isSame(date, 'day') && <div className={styles.today}>오늘</div>}
+          <span className={cx({ [styles.today]: isToday })}>{date.date()}</span>
         </div>
       )}
     </li>
