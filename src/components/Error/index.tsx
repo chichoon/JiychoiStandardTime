9b9945b976009fd721fbd7d404e14222db36139d
chichoon <chichoon.choi@gmail.com>
2022-07-11 15:57:27 +0900

    Design: 에러 화면 디자인 플레이어 디자인과 비슷하게 맞춤

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,3 +1,5 @@
+import cx from 'classnames';
+
 import { ErrorIcon } from 'assets/svgs';
 import styles from './error.module.scss';
 
@@ -8,8 +10,17 @@ interface IProps {
 const Error = ({ message }: IProps) => {
   return (
     <div className={styles.errorWrapper}>
-      <ErrorIcon />
-      <p className={styles.errorString}>{message}</p>
+      <div className={styles.errorComponentBox}>
+        <ErrorIcon />
+      </div>{' '}
+      <div className={styles.errorInformation}>
+        <p className={styles.playerDate}>#0 Invalid Date</p>
+        <div className={cx(styles.errorInfo, 'currentSongInfo')}>
+          <dt>Error!</dt>
+          <dd>오류!</dd>
+        </div>
+        <p className={styles.errorMessage}>{message}</p>
+      </div>
     </div>
   );
 };
