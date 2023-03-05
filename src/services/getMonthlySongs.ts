00eb4f0af404628375669b014ba395220c3d1c19
chichoon <chichoon.choi@gmail.com>
2023-03-06 00:25:01 +0900

    chore: :fire: 필요없을 것 같은 코드 일단 다 삭제 (재구조화 시작)

---
+++ src/services/getMonthlySongs.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { SongType } from 'types/musics';
+
+export function getMonthlySongs(year: string, month: string): () => Promise<SongType[]> {
+  return async function () {
+    const { data } = await axios.get(`/jst-songlist-${year}${month}`);
+    return data;
+  };
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,1 @@
-import getLatestSong from './getLatestSong';
-import getSongsListFiltered from './getSongsListFiltered';
-import getSongByIndex from './getSongByIndex';
-import getAllSongsByDay from './getAllSongsByDay';
-
-export { getLatestSong, getSongsListFiltered, getSongByIndex, getAllSongsByDay };
+export { getMonthlySongs } from './getMonthlySongs';
