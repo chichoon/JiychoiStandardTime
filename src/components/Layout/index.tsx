2e2223b958a070287e5b3c823ef7386c15e357ed
chichoon <chichoon.choi@gmail.com>
2023-06-14 23:55:11 +0900

    style: :lipstick: 이미지 삭제 및 그림자 효과 조절

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,41 +1,17 @@
-import { Suspense, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { useHoverDirty } from 'react-use';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { ErrorFallback, LoadingFallback } from 'components';
-import { useFetchAllSongs } from 'hooks';
-import { getIsPlaying } from 'states/isPlaying';
 import { NavSection } from './NavSection';
 import { FloatingButton } from './FloatingButton';
 
 import styles from './layout.module.scss';
 
 export const Layout = () => {
-  const imageRef = useRef(null);
-  const isPlaying = useSelector(getIsPlaying);
-  const isHovering = useHoverDirty(imageRef);
-  const navigate = useNavigate();
-  const songList = useFetchAllSongs();
-
-  function playerImage() {
-    if (isHovering) return '/images/question-image.png';
-    if (isPlaying) return '/images/playing-image.gif';
-    return '/images/main-image.gif';
-  }
-
-  function handleRandomButtonClick() {
-    const randomValue = Math.floor(Math.random() * (songList.length ?? 0));
-    navigate(`/play/${randomValue}`);
-  }
-
   return (
     <div className={styles.layoutBackground}>
       <div className={styles.layoutWrapper}>
-        <button type='button' onClick={handleRandomButtonClick}>
-          <img ref={imageRef} src={playerImage()} alt='main-profile' />
-        </button>
         <div className={styles.layoutContainer}>
           <header>
             <h1 className={styles.layoutTitle}>#지최표준시</h1>
