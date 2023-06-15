18f1d036ecaf37f5cd211d4b610a46d94b0cedb6
chichoon <chichoon.choi@gmail.com>
2023-06-15 16:16:10 +0900

    fix: :bug: MAX, MIN DATE 바깥에 있는 날짜에 접근 못하도록 막음

--- src/router/Calendar/OverviewCalendar/CalendarSelectMonth/index.tsx
+++ src/router/Calendar/OverviewCalendar/CalendarSelectMonth/index.tsx
@@ -12,11 +12,16 @@ interface Props {
   setIsSelectMonthOpen: Dispatch<SetStateAction<boolean>>;
 }
 
+function checkIsMonthUnavailable(date: Dayjs, month: number) {
+  if (date.month(month - 1).isAfter(MAXIMUM_DATE) || date.month(month - 1).isBefore(MINIMUM_DATE)) return true;
+  return false;
+}
+
 const CalendarSelectMonth = ({ date, setDate, setIsSelectMonthOpen }: Props) => {
   const monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   function handleSelectMonthButtonClick(month: number) {
-    if (date.month(month - 1).isAfter(MAXIMUM_DATE) || date.month(month - 1).isBefore(MINIMUM_DATE)) return;
+    if (checkIsMonthUnavailable(date, month)) return;
     setDate((prevState) => prevState.month(month - 1));
     setIsSelectMonthOpen(false);
   }
@@ -26,7 +31,10 @@ const CalendarSelectMonth = ({ date, setDate, setIsSelectMonthOpen }: Props) => 
       {monthList.map((value) => (
         <button
           key={`select-month-${value}`}
-          className={cx(styles.selectMonthButton, { [styles.isSelectedMonth]: date.month() + 1 === value })}
+          className={cx(styles.selectMonthButton, {
+            [styles.isSelectedMonth]: date.month() + 1 === value,
+            [styles.isUnavailable]: checkIsMonthUnavailable(date, value),
+          })}
           type='button'
           onClick={() => handleSelectMonthButtonClick(value)}
         >
